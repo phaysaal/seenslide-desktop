5586ce505b4c9326c3dd79fd63b33e775402a3fb
Mahmudul Faisal Al Ameen <mahmudulfaisal@gmail.com>
2026-04-02 00:53:19 +0200

    Release v1.0.26

--- seenslide/__init__.py
+++ seenslide/__init__.py
@@ -1,5 +1,5 @@
 """SeenSlide main package."""
 
-__version__ = "1.0.25"
+__version__ = "1.0.26"
 __author__ = "SeenSlide Team"
 __description__ = "Real-time slide navigation system for presentations"
